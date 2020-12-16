88ba293bfd86fa17b432411412bf46b733b457f1
ledungcobra <ledungcobra@gmail.com>
2020-12-16 13:35:21 +0700

    Pull master

--- src/app/controllers/siteController.js
+++ src/app/controllers/siteController.js
@@ -1,27 +1,42 @@
 const productModel = require('../models/productModel');
+const debug = require('debug')('HomePage');
+module.exports.index = async (req, res, next) => {
 
-module.exports.index = async(req, res, next) => {
-    res.render('index', {
-        layout: 'layouts/homePage',
-        title: 'Home'
-    });
+    try {
+        const galleryProducts = await productModel.getList(3, 12);
+        res.render('index', {
+            layout: 'layouts/homePage',
+            title: 'Home',
+            products: galleryProducts
+        });
+
+    } catch (e) {
+
+        debug(e);
+        res.render('site/404', {
+            title: '404',
+            pageName: e.toString()
+        });
+    }
+
+
 }
 
-module.exports.getAboutPage = async(req, res, next) => {
+module.exports.getAboutPage = async (req, res, next) => {
     res.render('site/about', {
         title: 'HDH Shoes',
         pageName: 'About'
     });
 }
 
-module.exports.getContactPage = async(req, res, next) => {
+module.exports.getContactPage = async (req, res, next) => {
     res.render('site/contact', {
         title: 'HDH Shoes',
         pageName: 'Contact'
     });
 }
 
-module.exports.search = async(req, res, next) => {
+module.exports.search = async (req, res, next) => {
     const keyword = req.query.keyword;
     let searchResultTitle = 'Không tìm thấy bất kỳ kết quả nào với từ khóa "' + keyword + '".';
     let limit = 12;
