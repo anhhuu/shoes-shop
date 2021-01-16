ffdac8ea48e98cf88d8f8f4ac411866d739b7d8b
anhhuu <panhhuu@gmail.com>
2021-01-16 21:08:47 +0700

    fix image profile

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -5,7 +5,6 @@ async function connect() {
     try {
         await mongoose.connect(
             process.env.DB_URI_V2, {
-                //'mmongodb+srv://anhhuu:QntiC4albYUOsYHz@shoes-shop-cluster.cdqes.mongodb.net/shoes-shop-dev-v2?retryWrites=true&w=majority', {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
                 useFindAndModify: false,
